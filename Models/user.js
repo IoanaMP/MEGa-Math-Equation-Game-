55a4239a38d87abe3d4754c3306265ef7c4011c7
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-06-01 23:38:07 +0300

    Models

---
+++ Models/user.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose')
+//un user va avea : email, parola , nume , prenume si rol
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      default: 'user',
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = model('users', userSchema)
