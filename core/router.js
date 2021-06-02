a663294d90585bfe8a2227596f9e15f221f430ef
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-06-03 00:39:00 +0300

    Routing attempt...

--- core/router.js
+++ core/router.js
@@ -1,16 +1,44 @@
-const url = require('url');
-const fs = require('fs').promises;
+var url = require('url');
+var fs = require('fs');
 
+function renderHTML(path, response) {
+    fs.readFile(path, null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
 
+module.exports = {
+  handleRequest: function(request, response) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      //console.log("request was made: " + req.url)
+      var path = url.parse(request.url).pathname;
+      switch (path) {
+          case '/':
+              renderHTML('./View/Pages/Homepage.html', response);
+              break;
+          case '/login':
+              renderHTML('./View/Pages/Login.html', response);
+              break;
+         case '/register':
+               renderHTML('./View/Pages/Register.html', response);
+               break;
+         case '/ecuatii':
+            renderHTML('./View/Pages/Ecuatii.html', response);
+            break;
+         case '/documentatie':
+            renderHTML('./View/Pages/Documentatie.html', response);
+            break;
+          default:
+              response.writeHead(404);
+              response.write('Route not defined');
+              response.end();
+      }
 
-// var url = req.url;
-// if(url ==='/about') {
-//     res.write(' Welcome to about us page'); 
-//     res.end(); 
-//  } else if(url ==='/contact') {
-//     res.write(' Welcome to contact us page'); 
-//     res.end(); 
-//  } else {
-//     res.write('Hello World!'); 
-//     res.end(); 
-//  }
+  }
+};
--- core/server.js
+++ core/server.js
@@ -1,20 +1,5 @@
-const http = require('http')
-const port = 3030
-const url = require('url');
-const fs = require('fs');
+var http = require('http');
+var app = require('./router');
 
-http.createServer(async (request, response) => {
-  response.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        fs.readFile('./View/index.html', null, function (error, data) {
-            if (error) {
-                response.writeHead(404);
-                response.write('Whoops! File not found!');
-            } else {
-                response.write(data);
-            }
-            response.end();
-        });
-  
-}).listen(port);
+http.createServer(app.handleRequest).listen(8000);
+console.log("Listening");
